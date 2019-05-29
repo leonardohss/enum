2efdf65d7ce9f130e6c21cc3b342389baa650422
Leonardo Sousa <leorick97@gmail.com>
2019-05-28 21:46:07 -0300

    Criação da classe Order e do enum OrderStatus, com os status dos pedidos (orders)

--- Enumeracao/Program.cs
+++ Enumeracao/Program.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+using System;
 
-namespace Enumeracao {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();/*conversão para string, incluindo na variavel */
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            /*conversão para string, necessario que o "delivered" seja 
+            identico ao definido no enum*/
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
         }
     }
 }
